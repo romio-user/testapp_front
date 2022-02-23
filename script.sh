14a193245c4b88097441a2606e9677054cb53c83
romio-user <romio-user>
2022-02-23 19:44:27 +0200

    Update JenkinsFile

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-aws ecs register-task-definition --cli-input-json file://front_task_definition.json
+#aws ecs register-task-definition --cli-input-json file://front_task_definition.json
+sed -i "s#REPOSITORY_URI#$REPOSITORY_URI#g" front_task_definition.json
+sed -i "s#IMAGE_TAG#$IMAGE_TAG#g" front_task_definition.json
+sed -i "s#EXECUTION_ROLE_ARN#${env.EXECUTION_ROLE_ARN}#g" front_task_definition.json
+sed -i "s#AWS_DEFAULT_REGION#${env.AWS_DEFAULT_REGION}#g" front_task_definition.json
